3a4bda1eb5db3cc9647bda91b744e98b0ca2883a
zhangyudong <977872239@qq.com>
2018-12-27 22:17:49 +0800

    个人电脑同步

--- app/src/main/java/com/example/totallearn/MainActivity.java
+++ app/src/main/java/com/example/totallearn/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.totallearn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 在个人电脑上进行同步
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
