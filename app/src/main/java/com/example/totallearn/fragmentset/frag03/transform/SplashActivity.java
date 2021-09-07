c8601b82429c56be9571088d6f89afa883866f7b
YuDongZhang <977872239@qq.com>
2021-09-07 20:10:50 +0800

    网易:canvas 旋转小球: 不动的时候小球的 位置 , 固定位置

---
+++ app/src/main/java/com/example/totallearn/fragmentset/frag03/transform/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.totallearn.fragmentset.frag03.transform;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.totallearn.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
