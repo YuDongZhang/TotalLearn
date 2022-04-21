19a49af5a42f61c4fe22f72cb2e1d340c7901240
张雨东 <zhangyudong@njpdxx.com>
2022-04-21 13:57:48 +0800

    网易 3.5.1 RxJava概念与观察者模式 , TestClient中运行可以看出来 , 观察者模式的理解

--- app/src/androidTest/java/com/example/totallearn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/totallearn/ExampleInstrumentedTest.java
@@ -19,9 +19,9 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+//        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.totallearn", appContext.getPackageName());
+//        assertEquals("com.example.totallearn", appContext.getPackageName());
     }
 
 
