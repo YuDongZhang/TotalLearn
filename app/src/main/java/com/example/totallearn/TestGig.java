eac8b8873720a2b70c6970681155ea643f6ca7ff
zhangyudong <977872239@qq.com>
2023-08-27 11:46:47 +0800

    主分支提交

--- app/src/main/java/com/example/totallearn/TestGig.java
+++ app/src/main/java/com/example/totallearn/TestGig.java
@@ -1,5 +1,15 @@
 package com.example.totallearn;
 
+import android.util.Log;
+
 public class TestGig {
+    public void test(){
+        Log.i("TAG", "Android ");
+
+    }
+    public void test1(){
+        Log.i("TAG", "test ");
+
+    }
 
 }
