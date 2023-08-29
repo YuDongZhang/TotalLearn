53bb97fa9e83f58a0486c978a7f07dcfbec9840a
zhangyd <zhangyd@yuntian.com>
2023-08-29 08:24:09 +0800

    测试修改

--- app/src/main/java/com/example/totallearn/TestGig.java
+++ app/src/main/java/com/example/totallearn/TestGig.java
@@ -3,8 +3,8 @@ package com.example.totallearn;
 import android.util.Log;
 
 public class TestGig {
-   public void test(){
-      Log.i("TAG", "test: ");
+    public void test(){
+        Log.i("TAG", "Android ");
 
     }
     public void test1(){
